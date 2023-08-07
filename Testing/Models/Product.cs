7a9129167865f49bf62446a13a54c95c2c230df3
Eric Caro <c.aroericm@gmail.com>
2023-08-07 15:09:21 -0500

    Update ASPNET

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Testing.Models
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
 {
     public class Product
     {
@@ -16,5 +18,8 @@
         public int OnSale { get; set; }
 
         public int StockLevel { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+
     }
 }
